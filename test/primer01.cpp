192d441a2f1c328e6ee6aeb61c8c053981ecf83b
chocho77 <100801684+chocho77@users.noreply.github.com>
2022-09-03 23:56:56 +0300

    Add files via upload

---
+++ test/primer01.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // operating with variables
+    // declaring variables 
+    int a,b;
+    int result;
+
+    //process:
+    a = 5;
+    b = 2;
+    a = a + 1;
+    result = a - b;
+
+    // print result to terminal
+    cout << result;   
+
+    cout << "\n"; // print new line
+    
+    // terminate the program
+
+    return 0;
+}
